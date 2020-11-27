c84129a9ee1bf232fcb491a6f2c4d6bab939282b
André Abadesso <andre.abadesso@gmail.com>
2020-11-27 14:10:54 -0300

    fix: update without WHERE on updateTimeLocks

--- src/db.ts
+++ src/db.ts
@@ -708,8 +708,10 @@ export const updateAddressLockedBalance = async (
                 WHERE \`address\` = ?
                   AND \`token_id\` = ?
                   AND \`locked\` = TRUE
-             )`,
-        [address, token]);
+             )
+           WHERE \`address\` = ?
+             AND \`token_id\` = ?`,
+        [address, token, address, token]);
       }
     }
   }
