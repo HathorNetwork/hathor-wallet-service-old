7212f377a9151d4d8a6391cbfd6795ad828d9a83
Pedro Ferreira <pedro@hathor.network>
2021-03-31 23:45:59 -0300

    fix: small fixes to finish library facade integration (#53)

--- src/api/txProposalCreate.ts
+++ src/api/txProposalCreate.ts
@@ -75,7 +75,7 @@ const bodySchema = Joi.object({
           .integer()
           .required()
           .min(0),
-      }).required(),
+      }),
     ),
   inputSelectionAlgo: Joi.string(),
 });
@@ -222,7 +222,7 @@ export const create: APIGatewayProxyHandler = async (event) => {
     // XXX We should store in address table the path of the address, not the index
     // For now we return the hardcoded path with only the address index as variable
     // The client will be prepared to receive any path when we add this in the service in the future
-    const addressPath = `m/44'/${hathorLib.constants.HATHOR_BIP44_CODE}'/0'/${addressDetail.index}`;
+    const addressPath = `m/44'/${hathorLib.constants.HATHOR_BIP44_CODE}'/0'/0/${addressDetail.index}`;
     return { txId: utxo.txId, index: utxo.index, addressPath };
   });
 
