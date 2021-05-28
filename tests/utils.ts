fcae2bfa8ef4f7fc382e0d841a1fca7b53c748dd
André Abadesso <andre.abadesso@gmail.com>
2021-05-28 15:40:24 -0300

    tests: passing tests

--- tests/utils.ts
+++ tests/utils.ts
@@ -57,6 +57,7 @@ export const cleanDatabase = async (mysql: ServerlessMysql): Promise<void> => {
     'token',
     'tx_proposal',
     'tx_proposal_outputs',
+    'transaction',
     'tx_output',
     'version_data',
     'wallet',
