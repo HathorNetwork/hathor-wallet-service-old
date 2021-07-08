0633bda1072f46a75b2c889a503d8ed917b020be
André Carneiro <andreluizmrcarneiro@gmail.com>
2021-07-07 22:16:58 -0300

    feat: seeders for mainnet and testnet

---
+++ seeders/testnet.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.bulkInsert('metadata', [{
+          key: 'height',
+          value: 0,
+        }], { transaction: t }),
+        queryInterface.bulkInsert('transaction', [{
+          tx_id: '000006cb93385b8b87a545a1cbb6197e6caff600c12cc12fc54250d39c8088fc',
+          height: 0,
+          timestamp: 1578075305,
+          version: 0,
+          voided: false,
+        }], { transaction: t }),
+        queryInterface.bulkInsert('tx_output', [{
+          tx_id: '000006cb93385b8b87a545a1cbb6197e6caff600c12cc12fc54250d39c8088fc',
+          index: 0,
+          token_id: '00',
+          address: 'HJB2yxxsHtudGGy3jmVeadwMfRi2zNCKKD',
+          value: 100000000000,
+        }], { transaction: t }),
+      ]);
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.bulkInsert('metadata', {
+          key: 'height',
+        }, { transaction: t }),
+        queryInterface.bulkInsert('transaction', {
+          tx_id: '000006cb93385b8b87a545a1cbb6197e6caff600c12cc12fc54250d39c8088fc',
+        }, { transaction: t }),
+        queryInterface.bulkInsert('tx_output', {
+          tx_id: '000006cb93385b8b87a545a1cbb6197e6caff600c12cc12fc54250d39c8088fc',
+          index: 0,
+        }, { transaction: t }),
+      ]);
+    });
+  },
+};
