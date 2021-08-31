65ca3b959b49ea0b544861941d35b78f90e5ab90
André Abadesso <andre.abadesso@gmail.com>
2021-08-31 15:38:59 -0300

    chore: added missing env variables on serverless environment config (#138)
    * chore: added missing env variables on serverless environment config
    
    * fix: old voided txs failure (#139)
    
    * fix: wrong attribute on fullnode response
    
    * fix: forcing the PRIMARY index on updateTxOutputSpentBy (#140)
    
    * fix: env vars during deploy
    
    * refactor: normalize production env variables for database migrations
    
    Co-authored-by: Luis Helder <luislhl@gmail.com>

--- db/config.js
+++ db/config.js
@@ -40,11 +40,11 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_ENDPOINT,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     dialectOptions: {
       bigNumberStrings: true,
