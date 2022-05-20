a769a0aeb22d75a6fcc8e03c8eb91ce5e968c01a
lucas3003 <lucas_5639@hotmail.com>
2022-05-19 22:36:14 -0300

    Add documentation on weight migration file

--- db/migrations/20220517191541-add-weight-to-tx.js
+++ db/migrations/20220517191541-add-weight-to-tx.js
@@ -4,6 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('transaction', 'weight', {
       type: Sequelize.FLOAT.UNSIGNED,
+      // We will temporaly support null values for weight until the first data migration is done.
+      // After that, we must remove support for null values on weight column.
       allowNull: true,
       defaultValue: null,
     });
