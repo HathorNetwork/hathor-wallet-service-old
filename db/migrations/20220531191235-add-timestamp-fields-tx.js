62596b0617288fea0e205ada4119d772e58e63b5
lucas3003 <lucas_5639@hotmail.com>
2022-06-06 23:08:25 -0300

    use transaction for creating the columns

--- db/migrations/20220531191235-add-timestamp-fields-tx.js
+++ db/migrations/20220531191235-add-timestamp-fields-tx.js
@@ -2,21 +2,37 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('transaction', 'created_at', {
-      type: 'TIMESTAMP',
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-    });
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addColumn('transaction', 'created_at', {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      });
 
-    await queryInterface.addColumn('transaction', 'updated_at', {
-      type: 'TIMESTAMP',
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
-    });
+      await queryInterface.addColumn('transaction', 'updated_at', {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      });
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   },
 
   down: async (queryInterface) => {
-    await queryInterface.removeColumn('transaction', 'created_at');
-    await queryInterface.removeColumn('transaction', 'updated_at');
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeColumn('transaction', 'created_at');
+      await queryInterface.removeColumn('transaction', 'updated_at');
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   }
 };
