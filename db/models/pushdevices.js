6c3a1e731214e32c6c9fd57178e43a3d92d7a047
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2022-11-09 18:28:39 +0000

    chore: remove unused param and fix lint claims

--- db/models/pushdevices.js
+++ db/models/pushdevices.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model, DataTypes
-} = require('sequelize');
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class PushDevices extends Model {
@@ -14,50 +12,54 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  PushDevices.init({
-    device_id: {
-      type: DataTypes.STRING(256),
-      allowNull: false,
-      primaryKey: true,
-    },
-    push_provider: {
-      type: DataTypes.ENUM(['ios', 'android']),
-      allowNull: false,
-      defaultValue: 'android',
-    },
-    wallet_id: {
-      type: DataTypes.STRING(64),
-      allowNull: false,
-      references: {
-        model: 'wallet',
-        key: 'id',
+  PushDevices.init(
+    {
+      device_id: {
+        type: DataTypes.STRING(256),
+        allowNull: false,
+        primaryKey: true,
+      },
+      push_provider: {
+        type: DataTypes.ENUM(['ios', 'android']),
+        allowNull: false,
+      },
+      wallet_id: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+        references: {
+          model: 'wallet',
+          key: 'id',
+        },
+      },
+      enable_push: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      enable_show_amounts: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      enable_only_new_tx: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: DataTypes.literal(
+          'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+        ),
       },
     },
-    enable_push: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
+    {
+      sequelize,
+      modelName: 'PushDevices',
+      tableName: 'push_devices',
+      timestamps: false,
     },
-    enable_show_amounts: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    enable_only_new_tx: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    updated_at: {
-      type: 'TIMESTAMP',
-      allowNull: false,
-      defaultValue: DataTypes.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
-    },
-  }, {
-    sequelize,
-    modelName: 'PushDevices',
-    tableName: 'push_devices',
-    timestamps: false,
-  });
+  );
   return PushDevices;
 };
