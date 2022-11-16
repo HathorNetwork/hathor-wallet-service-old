ced7b3e3b1e52040a8ae8fda2dc8103b0c95806b
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2022-11-16 20:37:19 +0000

    chore: remove file lint rule not necessary anymore

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-/* eslint-disable camelcase */
 import { strict as assert } from 'assert';
 import { ServerlessMysql } from 'serverless-mysql';
 import { get } from 'lodash';
