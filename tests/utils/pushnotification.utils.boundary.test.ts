52f0a31a67f016ad2fc75cb9d08f77479a01700e
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2022-11-23 00:48:49 +0000

    chore: add FCM boundary test

---
+++ tests/utils/pushnotification.utils.boundary.test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable jest/no-disabled-tests */
+import { PushNotificationUtils } from '@src/utils/pushnotification.utils';
+import { SendNotificationToDevice } from '@src/types';
+
+const registrationToken = 'fGw0qy4TGgk:APA91bGtWGjuhp4WRhHXgbabIYp1jxEKI08ofj_v1bKhWAGJQ4e3arRCW' +
+  'zeTfHaLz83mBnDh0aPWB1AykXAVUUGl2h1wT4XI6XazWpvY7RBUSYfoxtqSWGIm2nvWh2BOP1YG501SsRoE';
+const invalidRegistrationToken = 'invalid-registration-token';
+
+// This test was designed to make real call to FCM.
+// NOTE: Use a testing project. Do not use production project.
+describe('pushnotification.utils', () => {
+  describe('sendToFcm', () => {
+    it.skip('should call FCM', async () => {
+      expect.hasAssertions();
+
+      const deviceId = invalidRegistrationToken;
+      const title = 'New transaction';
+      const description = 'You have received 1 XYZ';
+      const txId = 'tx1';
+
+      const notification = {
+        deviceId,
+        title,
+        description,
+        metadata: { txId },
+      } as SendNotificationToDevice;
+
+      const result = await PushNotificationUtils.sendToFcm(notification);
+
+      expect(result).toStrictEqual({});
+    });
+  });
+});
