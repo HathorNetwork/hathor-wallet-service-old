1318a2708c90a52a56c7f7a1210d328d8067cdff
André Abadesso <andre.abadesso@gmail.com>
2022-11-24 13:18:46 -0300

    feat: check wallet addresses api (#305)
    * feat: added optional parameter to filter addresses
    
    * refactor: using filterAddresses as attribute
    
    * refactor: check address is now a new route instead of re-using the address route
    
    * feat: using Set for better performance
    
    * tests: added api tests for the checkMine api
    
    * refactor: limit the number of addresses changed to 512
    
    * tests: increased coverage on addresses
    
    * tests: added a test for invalid json
    
    * tests: stop overwriting index on ADDRESSES[3]
    
    * docs: added a comment explaining base58 regex

--- src/api/addresses.ts
+++ src/api/addresses.ts
@@ -7,6 +7,7 @@
 
 import 'source-map-support/register';
 
+import Joi from 'joi';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { ApiError } from '@src/api/errors';
 import { closeDbAndGetError, warmupMiddleware } from '@src/api/utils';
@@ -14,12 +15,82 @@ import {
   getWallet,
   getWalletAddresses,
 } from '@src/db';
+import { AddressInfo } from '@src/types';
 import { closeDbConnection, getDbConnection } from '@src/utils';
 import { walletIdProxyHandler } from '@src/commons';
 import middy from '@middy/core';
 import cors from '@middy/http-cors';
 
 const mysql = getDbConnection();
+
+const checkMineBodySchema = Joi.object({
+  addresses: Joi.array()
+    // Validate that addresses are a base58 string and exactly 34 in length
+    .items(Joi.string().regex(/^[A-HJ-NP-Za-km-z1-9]*$/).min(34).max(34))
+    .min(1)
+    .max(512) // max number of addresses in a tx (256 outputs and 256 inputs)
+    .required(),
+});
+
+/*
+ * Check if a list of addresses belong to the caller wallet
+ *
+ * This lambda is called by API Gateway on POST /addresses/check_mine
+ */
+export const checkMine: APIGatewayProxyHandler = middy(walletIdProxyHandler(async (walletId, event) => {
+  const status = await getWallet(mysql, walletId);
+
+  // If the wallet is not started or ready, we can skip the query on the address table
+  if (!status) {
+    return closeDbAndGetError(mysql, ApiError.WALLET_NOT_FOUND);
+  }
+
+  if (!status.readyAt) {
+    return closeDbAndGetError(mysql, ApiError.WALLET_NOT_READY);
+  }
+
+  const eventBody = (function parseBody(body) {
+    try {
+      return JSON.parse(body);
+    } catch (e) {
+      return null;
+    }
+  }(event.body));
+
+  const { value, error } = checkMineBodySchema.validate(eventBody, {
+    abortEarly: false,
+    convert: false,
+  });
+
+  if (error) {
+    const details = error.details.map((err) => ({
+      message: err.message,
+      path: err.path,
+    }));
+
+    return closeDbAndGetError(mysql, ApiError.INVALID_PAYLOAD, { details });
+  }
+
+  const sentAddresses = value.addresses;
+  const dbWalletAddresses: AddressInfo[] = await getWalletAddresses(mysql, walletId, sentAddresses);
+  const walletAddresses: Set<string> = dbWalletAddresses.reduce((acc, { address }) => acc.add(address), new Set([]));
+
+  await closeDbConnection(mysql);
+
+  const addressBelongMap = sentAddresses.reduce((acc: {string: boolean}, address: string) => {
+    acc[address] = walletAddresses.has(address);
+
+    return acc;
+  }, {});
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      success: true,
+      addresses: addressBelongMap,
+    }),
+  };
+})).use(cors());
 
 /*
  * Get the addresses of a wallet
