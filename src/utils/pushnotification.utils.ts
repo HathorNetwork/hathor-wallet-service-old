18176d18479965bb10224d155ceeefe64beffef5
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2022-12-19 19:33:47 +0000

    chore: json as object and use single quote

--- src/utils/pushnotification.utils.ts
+++ src/utils/pushnotification.utils.ts
@@ -75,16 +75,16 @@ const FIREBASE_AUTH_PROVIDER_X509_CERT_URL = process.env.FIREBASE_AUTH_PROVIDER_
 const FIREBASE_CLIENT_X509_CERT_URL = process.env.FIREBASE_CLIENT_X509_CERT_URL;
 
 const serviceAccount = {
-  "type": "service_account",
-  "project_id": FIREBASE_PROJECT_ID,
-  "private_key_id": FIREBASE_PRIVATE_KEY_ID,
-  "private_key": FIREBASE_PRIVATE_KEY,
-  "client_email": FIREBASE_CLIENT_EMAIL,
-  "client_id": FIREBASE_CLIENT_ID,
-  "auth_uri": FIREBASE_AUTH_URI,
-  "token_uri": FIREBASE_TOKEN_URI,
-  "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL,
+  type: 'service_account',
+  project_id: FIREBASE_PROJECT_ID,
+  private_key_id: FIREBASE_PRIVATE_KEY_ID,
+  private_key: FIREBASE_PRIVATE_KEY,
+  client_email: FIREBASE_CLIENT_EMAIL,
+  client_id: FIREBASE_CLIENT_ID,
+  auth_uri: FIREBASE_AUTH_URI,
+  token_uri: FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
 };
 
 initializeApp({
