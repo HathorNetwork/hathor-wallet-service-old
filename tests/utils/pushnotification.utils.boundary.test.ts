817a5bf5115db1e4d37572d5b8ab4de00e154674
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2022-12-29 15:55:51 +0000

    fix: import default fcmAdmin to invoke initializeApp (#344)

--- tests/utils/pushnotification.utils.boundary.test.ts
+++ tests/utils/pushnotification.utils.boundary.test.ts
@@ -2,18 +2,17 @@
 import { PushNotificationUtils } from '@src/utils/pushnotification.utils';
 import { SendNotificationToDevice } from '@src/types';
 
-const registrationToken = 'fGw0qy4TGgk:APA91bGtWGjuhp4WRhHXgbabIYp1jxEKI08ofj_v1bKhWAGJQ4e3arRCW'
-  + 'zeTfHaLz83mBnDh0aPWB1AykXAVUUGl2h1wT4XI6XazWpvY7RBUSYfoxtqSWGIm2nvWh2BOP1YG501SsRoE';
+const registrationToken = 'fR4TqxCASDCtV1sWs53ZO0:APA91bE0lw-t44auU5AS0-cj3qcUiIH_bGY8m_TwnNvnx5xEus3yPYy-95zmz-h6PNp9tKFazyfu0wb7E0o_zqjR3eqP3Q1lSMbgK9X6NQIUAhkLtAmDE1LaEoN7ql1p18XPHQClM5P0';
 const invalidRegistrationToken = 'invalid-registration-token';
 
 // This test was designed to make real call to FCM.
 // NOTE: Use a testing project. Do not use production project.
 describe('pushnotification.utils', () => {
   describe('sendToFcm', () => {
-    it.skip('should call FCM', async () => {
+    it('should call FCM', async () => {
       expect.hasAssertions();
 
-      const deviceId = invalidRegistrationToken;
+      const deviceId = registrationToken;
       const title = 'New transaction';
       const description = 'You have received 1 XYZ';
       const txId = 'tx1';
