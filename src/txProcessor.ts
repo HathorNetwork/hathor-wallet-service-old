ff9a6a219abe813e661e371a02cce60c49e03890
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-01-03 21:14:02 +0000

    fix: disable push notification on new tx

--- src/txProcessor.ts
+++ src/txProcessor.ts
@@ -154,11 +154,14 @@ export const onNewTxRequest: APIGatewayProxyHandler = async (event, context) => 
       .catch((err) => logger.error('[ALERT] Errored on nftHandlerLambda invocation', err));
   }
 
-  const walletBalanceMap = await getWalletBalancesForTx(mysql, tx);
-  const { length: hasAffectWallets } = Object.keys(walletBalanceMap);
-  if (hasAffectWallets) {
-    PushNotificationUtils.invokeOnTxPushNotificationRequestedLambda(walletBalanceMap)
-      .catch((err) => logger.error('[ALERT] Errored on invokeOnTxPushNotificationRequestedLambda invocation', err));
+  const pushNotificationEnabled = false;
+  if (pushNotificationEnabled) {
+    const walletBalanceMap = await getWalletBalancesForTx(mysql, tx);
+    const { length: hasAffectWallets } = Object.keys(walletBalanceMap);
+    if (hasAffectWallets) {
+      PushNotificationUtils.invokeOnTxPushNotificationRequestedLambda(walletBalanceMap)
+        .catch((err) => logger.error('[ALERT] Errored on invokeOnTxPushNotificationRequestedLambda invocation', err));
+    }
   }
 
   return {
