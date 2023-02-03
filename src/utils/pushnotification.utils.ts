f5919e1cdf2a46f18b92392e81ff76ced4ce2859
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-02-03 21:37:49 +0000

    chore: test firebase env

--- src/utils/pushnotification.utils.ts
+++ src/utils/pushnotification.utils.ts
@@ -119,6 +119,8 @@ const serviceAccount = {
 let firebaseInitialized = false;
 if (isPushNotificationEnabled()) {
   try {
+    console.log('serviceAccount.private_key', serviceAccount.private_key);
+    console.log('FIREBASE_PRIVATE_KEY', FIREBASE_PRIVATE_KEY);
     fcmAdmin.initializeApp({
       credential: credential.cert(serviceAccount as ServiceAccount),
       projectId: FIREBASE_PROJECT_ID,
