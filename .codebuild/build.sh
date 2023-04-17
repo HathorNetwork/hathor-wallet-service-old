a1679e83d423c60563059bccb0487053d677107e
Luis Helder <luislhl@gmail.com>
2023-04-17 17:55:31 -0300

    chore: improve rollback mechanism (#410)
    * chore: support new mainnet-staging environment
    
    * chore: improve our rollback mechanism
    
    * chore: improve our rollback mechanism
    
    * chore: change deploy channel reference to wallet-service-deploys
    
    * chore: allow rollback individually for testnet and mainnet
    
    ---------
    
    Co-authored-by: Alex Ruzenhack <alex.ruzenhack@gmail.com>

--- .codebuild/build.sh
+++ .codebuild/build.sh
@@ -1,15 +1,44 @@
 set -e;
 
 send_slack_message() {
-    DEPLOYED_VERSION=$1;
+    MESSAGE=$1;
 
     curl -H "Content-type: application/json" \
-        --data "{\"channel\":\"${SLACK_DEPLOYS_CHANNEL_ID}\",\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Hathor Wallet Service*\nNew version deployed: ${DEPLOYED_VERSION}\"}}]}" \
+        --data "{\"channel\":\"${SLACK_DEPLOYS_CHANNEL_ID}\",\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Hathor Wallet Service*\n${MESSAGE}\"}}]}" \
         -H "Authorization: Bearer ${SLACK_OAUTH_TOKEN}" \
         -X POST https://slack.com/api/chat.postMessage;
 }
 
 echo "Building git ref ${GIT_REF_TO_DEPLOY}..."
+
+exit=false;
+
+# Checks whether there is a file called "rollback_mainnet_production", which is used by our other CodeBuild to indicate that this is a mainnet-production rollback
+if [ -f "rollback_mainnet_production" ]; then
+    # Gets all env vars with `mainnet_` prefix and re-exports them without the prefix
+    for var in "${!mainnet_@}"; do
+        export ${var#mainnet_}="${!var}"
+    done
+    make deploy-lambdas-mainnet;
+    send_slack_message "Rollback performed on mainnet-production to: ${GIT_REF_TO_DEPLOY}";
+    exit=true;
+fi;
+
+# Checks whether there is a file called "rollback_testnet_production", which is used by our other CodeBuild to indicate that this is a testnet-production rollback
+if [ -f "rollback_testnet_production" ]; then
+    # Gets all env vars with `testnet_` prefix and re-exports them without the prefix
+    for var in "${!testnet_@}"; do
+        export ${var#testnet_}="${!var}"
+    done
+    make deploy-lambdas-testnet;
+    send_slack_message "Rollback performed on testnet-production to: ${GIT_REF_TO_DEPLOY}";
+    exit=true;
+fi;
+
+if [ "$exit" = true ]; then
+  echo "Rollbacks performed successfully. Exiting now.";
+  exit 0;
+fi
 
 if expr "${GIT_REF_TO_DEPLOY}" : "master" >/dev/null; then
     # Gets all env vars with `testnet_` prefix and re-exports them without the prefix
@@ -25,7 +54,7 @@ elif expr "${GIT_REF_TO_DEPLOY}" : "v[0-9]\+\.[0-9]\+\.[0-9]\+-rc\.[0-9]\+" >/de
     done
     make migrate;
     make deploy-lambdas-mainnet-staging;
-    send_slack_message "${GIT_REF_TO_DEPLOY}"
+    send_slack_message "New version deployed to mainnet-staging: ${GIT_REF_TO_DEPLOY}"
 elif expr "${GIT_REF_TO_DEPLOY}" : "v.*" >/dev/null; then
     # Gets all env vars with `mainnet_` prefix and re-exports them without the prefix
     for var in "${!mainnet_@}"; do
@@ -33,7 +62,7 @@ elif expr "${GIT_REF_TO_DEPLOY}" : "v.*" >/dev/null; then
     done
     make migrate;
     make deploy-lambdas-mainnet;
-    send_slack_message "${GIT_REF_TO_DEPLOY}"
+    send_slack_message "New version deployed to mainnet-production: ${GIT_REF_TO_DEPLOY}"
 else
     # Gets all env vars with `dev_` prefix and re-exports them without the prefix
     for var in "${!dev_@}"; do
