94b85022fd37425bec7158b2182e68d8d866d239
André Abadesso <andre.abadesso@gmail.com>
2023-07-28 11:00:16 -0300

    tests: using jest fake timers

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  timers: 'fake',
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1',
     '^@tests/(.*)$': '<rootDir>/tests/$1',
